28dbccbe1808b010a0048021fee2ed16423f760d
Suhas K Vittal <svittal8@ampere09.cc.gatech.edu>
2025-04-04 03:39:58 -0400

    results complete for all but stream

--- scripts/all_collects.sh
+++ scripts/all_collects.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-python3 scripts/collect_stats.py baseline data/baseline.csv
-python3 scripts/collect_stats.py wcache data/wcache.csv
+#python3 scripts/collect_stats.py baseline data/baseline.csv
+#python3 scripts/collect_stats.py wcache data/wcache.csv
+
+python3 scripts/collect_stats.py baseline_open data/baseline_open.csv
+python3 scripts/collect_stats.py baseline_close data/baseline_close.csv
+
+python3 scripts/collect_stats.py wcache_open data/wcache_open.csv
+python3 scripts/collect_stats.py wcache_close data/wcache_close.csv
